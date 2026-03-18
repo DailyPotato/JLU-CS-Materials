64e6e287fb08fe3b0f2556e8bddc1e7a7a545cd7
Ciallo <yudaily0110@163.com>
2026-03-19 02:40:59 +0800

    Revert "add oop homework2"
    This reverts commit 78cbdf480e7ae3bc6e1d78ed86f14a31658b2f5b.

--- 大二上/oop/homework2/geometry.hpp
+++
@@ -1,9 +0,0 @@
-# ifndef GEOMETRY_HPP
-# define GEOMETRY_HPP
-
-# include "constants.hpp"
-extern int rect_count;
-extern int tri_count;
-float rectangleArea(int a,int b);
-float triangleArea(int a,int b);
-# endif
--- 大二上/oop/homework2/utils.hpp
+++
@@ -1,8 +0,0 @@
-# ifndef UTILS_H
-# define UTILS_H
-
-void printSeparator();
-
-void printStatistics(int addCount ,int subCount, int mulCount, int rectCount, int triCount);
-
-# endif
