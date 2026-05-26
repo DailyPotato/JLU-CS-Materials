48bcc0d3fa1e601f8c2f62bbe0a8d7c4debe5ccd
Ciallo <yudaily0110@163.com>
2026-05-26 23:28:09 +0800

    Q2

--- 大一下/oop/上机测试3/Q2/A2.cpp
+++ 大一下/oop/上机测试3/Q2/A2.cpp
@@ -1,19 +1,56 @@
 #include <iostream>
+#include <cmath>
+#include <utility>
 using namespace std;
 
-class Bike
+class CTNFRACTION
 {
+public:
+    CTNFRACTION(double n)
+    {
+        double temp=n;
+        while(temp>1e-6)
+        {
+            arr[len++]=static_cast<int>(temp);
+            temp=1/(temp-arr[len-1]);
+        }
+    }
+    int geta_i(int i)
+    {
+        if(i>=len) return -1;
+        return arr[i];
+    }
+    pair<int, int> calFraction(int q)
+    {
+        int numerator=1;
+        int denominator=1;
+        for(int i=q-1;i>=0;i--)
+        {
+            if(i==len-1)
+            {
+                numerator=arr[i];
+                denominator=1;
+            }
+            else
+            {
+                int temp=denominator;
+                denominator=arr[i]*denominator+numerator;
+                numerator=temp;
+            }
+        }
+        return make_pair(numerator, denominator);
+    }
+
 private:
-    int a;
+    int MAXLEN=30;
+    int arr[30];
+    int len=0;
 };
 
-class Player:private Bike
-{
-    int b;
-};
 int main()
 {
-    Player player;
-    Bike*pb=(Bike*)(&player);
+    double n;
+    cin>>n;
+    CTNFRACTION ctn(n);
     return 0;
 }
